041f181de16023a4d7518dbd008dfa6c7a8ed69d
Daneil Wei <daneilmu1@gmail.com>
2014-05-22 18:04:29 +0800

    add user/new html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create!(user_params)
+  end
+
+
+  private
+    def user_params
+      params.permit(:id, :name, :password, :re_password, :company, 
+        :contact_email, :created_at)
+    end
 end
