04debaa6d5c402cc52a2030bd0117eb4e41de579
Daneil Wei <daneilmu1@gmail.com>
2014-05-23 11:53:03 +0800

    add bcrypt-ruby for secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
+  include BCrypt
+  validates_presence_of :name, :password, :re_password, :company, :contact_email
+  validates :contact_email, uniqueness: true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+  validates :password, length: { minimum: 6}
+
+  before_create :init_password
+
+  private
+
+  def init_password
+    raise "两次密码输入不一致" unless self.password == self.re_password
+    self.password = BCrypt::Password.create(self.password)
+    self.re_password = BCrypt::Password.create(self.re_password)
+
+  end
 end
