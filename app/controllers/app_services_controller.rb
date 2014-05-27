27b2db138f542e2a4dfcdcd6302e397382010a76
Daneil Wei <daneilmu1@gmail.com>
2014-05-27 12:07:58 +0800

    add select some services with default true

--- app/controllers/app_services_controller.rb
+++ app/controllers/app_services_controller.rb
@@ -4,18 +4,28 @@ class AppServicesController < ApplicationController
     params['service_ids'].each do |id|
       AppService.create!(service_id: id, app_id: @app.id)
     end
+    redirect_to app_app_services_url, app_id: @app.id
   end
 
   def edit
     @app_service = AppService.find(params[:id])
   end
+
   def index
-    @app_service = AppService.all
+    @app_service = AppService.where(app_id: params[:app_id])
+    @app = App.find(params[:app_id])
   end
+
   def new
     @app = App.find(params[:app_id])
     @app_service = AppService.new
     @services = Service.all
+  end
+  
+  def destroy
+    @app_service = AppService.find(params[:id])
+    @app_service.destroy
+    redirect_to app_app_services_url, app_id: params[:app_id]
   end
 
   private
