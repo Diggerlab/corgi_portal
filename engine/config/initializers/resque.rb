7d0cb20ca1bdc2bafff12812c7c5bdf11f956afb
Daneil Wei <daneilmu1@gmail.com>
2014-06-13 17:33:46 +0800

    load resque.yaml

--- engine/config/initializers/resque.rb
+++ engine/config/initializers/resque.rb
@@ -3,5 +3,5 @@ require 'resque/server'
 rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
 rails_env = Rails.env || 'development'
 
-# resque_config = YAML.load_file(File.dirname(__FILE__) + '../resque.yml')
+resque_config = YAML.load_file(File.dirname(__FILE__) + '/../resque.yml')
 Resque.redis = resque_config[rails_env]
